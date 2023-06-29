b75a094290cb19ebaa14215fe912b16658cf231f
chit ko <80560570+chitkoko7214084@users.noreply.github.com>
2023-06-29 15:29:20 -0400

    Create main.py
    This program will generate a brand name for you.

---
+++ main.py
@@ -0,0 +1,11 @@
+#1. Create a greeting for your program.
+print ("Welcome for brand name program")
+#2. Ask the user for the city that they grew up in.
+name = input ("What the city that you grew up in?\n") 
+
+#3. Ask the user for the name of a pet.
+pet =input ("What is the name of a pet?\n")
+#4. Combine the name of their city and pet and show them their band name.
+Band_name = (name+pet)
+#5. Make sure the input cursor shows on a new line:
+print (Band_name)
